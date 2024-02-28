3ef04cbd56442fb9506c38adcc290528c92a0f9f
Shiva-Dutt <shivasdr154@gmail.com>
2024-02-28 10:40:32 +0530

    Updated index.css

--- src/App.jsx
+++ src/App.jsx
@@ -22,14 +22,16 @@ function App() {
   }, [])
   
   return !loading ? (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-      <div className='w-full block'>
+    <div className='min-h-screen flex flex-wrap content-between text-white bg-[#00040F] '>
+      <div className="w-full block">
         <Header />
         <main>
-        TODO:  <Outlet />
+          <Outlet/>
         </main>
         <Footer />
       </div>
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[30%] h-[30%] -left-1/2 bottom-0 rounded-full blue__gradient" />
     </div>
   ) : null
 }
