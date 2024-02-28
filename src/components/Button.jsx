6146520672eedd30ff6e4f439bf7ef332ff81361
Shiva-Dutt <shivasdr154@gmail.com>
2024-02-28 09:44:33 +0530

    Updated Footer and added svg's

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 function Button({
     children,
     type = 'button',
-    bgColor = 'bg-blue-600',
-    textColor = 'text-white',
-    className = '',
+    bgColor = "",
+    textColor = "",
+    className = "",
     ...props
 }) {
   return (
     <div>
-      <button className={`px-4 py-2 rounded-lg ${className} ${bgColor} ${textColor}`} {...props}>
+      <button className={`px-2 py-2 ${className} ${bgColor} ${textColor}`} type= {type} {...props}>
       {children}
       </button>
     </div>
