cf1b116d3fa6f3f1759fbfbb493749b3f921a9cb
Shiva-Dutt <shivasdr154@gmail.com>
2024-02-28 10:02:49 +0530

    Added App Icon & Updated Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,8 +12,10 @@ function Login() {
     const dispatch = useDispatch()
     const { register, handleSubmit } = useForm()
     const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
 
     const login = async (data) => {
+        setLoading(true)
         setError('')
         try {
             const session = await authService.login(data)
@@ -23,30 +25,31 @@ function Login() {
                 if (userData) {
                     dispatch(authLogin(userData))
                 }
-                navigate("/")
-            } else {
-
+                navigate("/all-posts")
             }
         } catch (error) {
             setError(error.message)
+        } finally {
+            setLoading(false)
         }
     }
 
     return (
         <div
-            className='flex items-center justify-center w-full py-8'>
-            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+            className='flex items-center justify-center md:min-h-[80vh]'
+        >
+    <div className={`mx-auto w-[85%] md:w-full md:max-w-[28rem]  rounded-xl p-5 md:p-10   border-2 border-[#33BBCF]`}>
                 <div className="mb-2 flex justify-center">
                     <span className="inline-block w-full max-w-[100px]">
                         <Logo width="100%" />
                     </span>
                 </div>
                 <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
-                <p className="mt-2 text-center text-base text-black/60">
+                <p className="mt-2 text-center text-base text-white/60">
                     Don&apos;t have any account?&nbsp;
                     <Link
                         to="/signup"
-                        className="font-medium text-primary transition-all duration-200 hover:underline"
+                        className="font-medium text-white/80 hover:text-white transition-all duration-200 hover:underline"
                     >
                         Sign Up
                     </Link>
@@ -61,7 +64,8 @@ function Login() {
                             {...register("email", {
                                 required: true,
                                 validate: {
-                                    matchPattern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || "Email address must be a valid address"
+                                    matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                                        "Email address must be a valid address",
                                 }
                             })}
                         />
@@ -73,10 +77,13 @@ function Login() {
                                 required: true,
                             })}
                         />
+                        {loading? 
+                        <div className='w-full grid place-items-center'> <Loader></Loader></div>
+                        :
                         <Button
                             type="submit"
-                            className="w-full"
-                        >Sign in</Button>
+                            className=" my-4 py-2 px-5 w-full text-black font-semibold button-custom rounded-xl shadow-lg   hover:cursor-pointer"
+                        >Sign in</Button>}
                     </div>
                 </form>
             </div>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-
+import logo from "/blogit-logo.png"
 function Logo({width = '100px'}) {
   return (
-    <div>
-      Logo
-    </div>
+    <div className='border-white  rounded-sm'><img src={logo} width={width} alt="" /></div>
   )
 }
 
